2ebb4836583805c50a02f1a801d1b3d0fe8998e1
taisei <taisei25864@gmail.com>
2023-04-20 00:01:55 +0900

    add nomeaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [data, setData] = React.useState();
+	const url = "http://127.0.0.1:8000";
+
+	const GetData = () => {
+		axios.get(url).then((res) => {
+			setData(res.data);
+		});
+	};
+	return (
+		<div>
+			<div>ここに処理を書いていきます</div>
+			{data ? <div>{data["Hello"]}</div> : <button onClick={GetData}>データを取得</button>}
+		</div>
+	);
 }
 
 export default App;
